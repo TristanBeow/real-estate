06bb2d67c83d231742b1001d06243dafd879e47c
TristanBeow <TheDude@MacBook-Pro-de-Tristan.local>
2017-02-21 14:59:41 +0100

    Tristan Nicolaides VF

--- app.js
+++ app.js
@@ -15,9 +15,17 @@ app.use( '/assets', express.static( 'assets' ) );
 
 //makes the server respond to the '/' route and serving the 'home.ejs' template in the 'views' directory
 app.get( '/', function ( req, res ) {
-    res.render( 'home', {
-        message: 'The Home Page!'
-    });
+    var URL = req.query.urlLBC
+
+    if ( URL ) {
+        callLBC( URL, res )
+    }
+    else {
+
+        res.render( 'home', {
+            message: 'Comparateur de prix pour Appartement'
+        });
+    }
 });
 
 
@@ -25,3 +33,58 @@ app.get( '/', function ( req, res ) {
 app.listen( 3000, function () {
     console.log( 'App listening on port 3000!' );
 });
+
+function callLBC( _url, res ) {
+    request( _url, function ( error, response, body ) {
+        if ( !error && response.statusCode == 200 ) {
+            var url = cheerio.load( body )
+            var firstPrice = url( 'span.value' ).eq( 0 ).text()
+
+            var price = url( 'span.value' ).eq( 0 ).text().replace( '€', '' )
+            price = price.replace( / /g, "" )
+
+
+
+            console.log( price )
+            var nomVille = url( 'span.value' ).eq( 1 ).text().split( ' ' )[0]
+            var codePostal = url( 'span.value' ).eq( 1 ).text().split( ' ' )[1]
+            var typeDeBien = url( 'span.value' ).eq( 2 ).text()
+            var surface = url( 'span.value' ).eq( 4 ).text().split( ' ' )[0]
+            console.log( surface )
+            var prixAuMetreCarre = price / surface
+            console.log( prixAuMetreCarre )
+
+
+            request( 'http://www.meilleursagents.com/prix-immobilier/' + nomVille.toLowerCase() + '-' + codePostal, function ( error, response, body ) {
+                if ( !error && response.statusCode == 200 ) {
+                    var url2 = cheerio.load( body )
+                    var prixMoyenAppartement = url2( 'div.small-4.medium-2.columns.prices-summary__cell--median' ).eq( 0 ).text().replace( '€', '' ).replace( /\s/g, '' )
+                    prixMoyenAppartement = parseFloat( prixMoyenAppartement )
+                    var prixMoyenMaison = url2( 'div.small-4.medium-2.columns.prices-summary__cell--median' ).eq( 1 ).text().replace( '€', '' ).replace( /\s/g, '' )
+                    var message = ' '
+                    if ( typeDeBien == 'Appartement' ) {
+                        if ( prixMoyenAppartement > prixAuMetreCarre ) {
+                            message = 'Bon deal, le prix moyen au mètre carré de cet appartement est inférieur au prix moyen du mètre carré de la région'
+                        }
+                        else {
+                            message = 'Mauvais deal, le prix moyen au mètre carré de cet appartement est supérieur au prix moyen du mètre carré de la région'
+                        }
+                    }
+                    else {
+                        if ( prixMoyenMaison > prixAuMetreCarre ) {
+                            message = 'Bon deal, le prix moyen au mètre carré de cette maison est inférieur au prix moyen du mètre carré de la région'
+                        }
+                        else {
+                            message = 'Mauvais deal, le prix moyen au mètre carré de cette maison est supérieur au prix moyen du mètre carré de la région'
+                        }
+
+                    }
+
+                    res.render( 'home', {
+                        message: message
+                    });
+                }
+            })
+        }
+    })
+}
